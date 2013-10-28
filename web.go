ee6e53ed7483a17477b9558097c7e55fcda438ba
Jackong <JackongC@gmail.com>
2013-10-28 11:46:10 +0800

    refactor web

--- web.go
+++ web.go
@@ -17,7 +17,7 @@ import (
 
 
 func Go() {
-	log.Info("going")
+	log.Info("Starting server...")
 	for _, path := range config.Project["view"]["paths"].([]interface {}) {
 		http.Handle(path.(string), http.FileServer(http.Dir(config.Project["view"]["root"].(string))))
 	}
@@ -26,29 +26,32 @@ func Go() {
 }
 
 func handler(writer http.ResponseWriter, req *http.Request) {
-	log.Info("request")
+	//todo log
 	defer func() {
 		if e := recover(); e != nil {
 			err := e.(err.Input)
-			fmt.Println(err)
+			log.Error(err)
 			http.Error(writer, "400 Bad Request", http.StatusBadRequest)
 			return
 		}
 	}()
 
 	if !router.IsSupportMethod(req.Method) {
+		log.Error("Not support method: ", req.Method)
 		http.Error(writer, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	before, rt := router.GetRouter(req.Method, req.URL.Path)
 	if rt == nil {
+		log.Error("redirct to /html/", req.URL.Path)
 		http.Redirect(writer, req, "/html" + req.URL.Path, http.StatusFound)
 		return
 	}
 
 	input := input.New(req)
 	if ok := before.IsForward(input); !ok {
+		log.Error("Can not forward")
 		http.Error(writer, "400 Bad Request", http.StatusBadRequest)
 		return
 	}
